062e9d3cf30d0ee1c9f46722783f6d1ec53be911
Rithik2211 <rithikpranao22@gmail.com>
2024-12-31 00:21:18 +0530

    updated react router dom code in front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-blue-500">
+      <h1 className="text-3xl font-bold text-blue-500">
         Hello world!
       </h1>
     </>
