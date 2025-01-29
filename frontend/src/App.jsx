a12e6dd4cb82ebd6cd5df546a0b140d943797285
Rithik2211 <rithikpranao22@gmail.com>
2025-01-29 06:37:30 +0530

    updated app code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500">
-        Hello world!
+        Book app
       </h1>
+      <HomePage />
     </>
   )
 }
