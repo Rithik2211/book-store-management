d1c459145a1bb443bbd57dbd0f190ac928ae19a5
Rithikpranao_22 <rithikpranao22@gmail.com>
2025-02-09 16:19:12 +0530

    feature/added-navbar-ui (#1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <>
-      <nav>Navigation</nav>
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+      {/* <nav></nav> */}
+      <NavigationBar />
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
         <Outlet />
       </main>
       <footer>Footer</footer>
