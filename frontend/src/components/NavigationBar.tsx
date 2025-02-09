32b75443ccc5ad198b8f3baf8d77da8d7d97b14f
Rithik2211 <rithikpranao22@gmail.com>
2025-02-09 16:18:32 +0530

    feature/added-navbar-ui

---
+++ frontend/src/components/NavigationBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const NavigationBar = () => {
+  return (
+    <div className='flex flex-row h-[55px] max-w-screen w-full bg-white border-b-[1.5px] shadow-blue-100-md justify-between items-center text-black fixed top-0 left-0 z-10 px-[60px]'>
+        <div className='flex flex-row justify-between items-center gap-[30px] w-[350px]'>
+            <img src='/Menu.svg' alt='title logo' className='w-5 h-5'/>
+            <div className='hidden md:block'>
+                <div className=' bg-[#EAEAEA] rounded-[5px] px-4 flex flex-row gap-[10px] relative w-[250px]'>
+                    <img src='/Search.svg' alt='basket' className='w-3 h-6'/>
+                    <input type='text' placeholder='What are you looking for ?' className='bg-transparent border-none w-full outline-none py-0 text-sm text-[#828282]' />
+                </div>
+            </div>
+        </div>
+        <div className='flex flex-row justify-between items-center w-auto gap-[15px] md:gap-[30px]'>
+            <img src='/Account.svg' alt='account' className='w-4 h-6'/>
+            <img src='/Favorite.svg' alt='favorite' className='w-4 h-6'/>
+            <div>
+                <button className='bg-button text-white px-5 py-1 rounded-[8px] flex flex-row gap-1 md:gap-3 text-sm'><img src='/basket.svg' alt='basket' className='w-3 h-5'/>Basket</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavigationBar;
