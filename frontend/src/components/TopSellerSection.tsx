144dc2c26ce698dc8bf9aaf91c22f48ee9df0f8e
Rithik2211 <rithikpranao22@gmail.com>
2025-02-16 22:46:48 +0530

    updated dropdown container

--- frontend/src/components/TopSellerSection.tsx
+++ frontend/src/components/TopSellerSection.tsx
@@ -1,8 +1,53 @@
+import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import React from 'react'
 
 const TopSellerSection = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  const dropdown = [
+    {
+      value: 'fiction',
+      label: 'Fiction',
+    },
+    {
+      value: 'romance',
+      label: 'Romance',
+    },
+    {
+      value: 'mystery',
+      label: 'Mystery',
+    },
+    {
+      value: 'horror',
+      label: 'Horror',
+    },
+  ]
+
   return (
-    <div>TopSellerSection</div>
+    <div className='flex flex-col justify-between items-center max-w-7xl w-full  gap-5 lg:gap-[180px]'>
+      <FormControl className='w-[100px] border-none'>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          {
+            dropdown?.map((item, index) => (
+              <MenuItem  key={index} value={item.value}>{item.label}</MenuItem>
+            ))
+          }
+        </Select>
+      </FormControl>
+      <div className=''>
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderIntro from '../components/HeaderIntro'
 import ContactUs from '../components/ContactUs'
+import TopSellerSection from '../components/TopSellerSection'
 
 const HomePage = () => {
     
   return (
     <div className='max-w-screen-2xl w-full min-h-screen py-8'>
         <HeaderIntro />
+        <TopSellerSection />
         <ContactUs />
     </div>
   )
