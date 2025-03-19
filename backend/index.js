8b690404abf7500958f53185114773c963ed3c2b
Rithik2211 <rithikpranao22@gmail.com>
2025-03-20 01:05:39 +0530

    updated changes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env || 5000;
+const port = process.env.PORT || 5001;
 
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    app.get('/', (req, res)=>{
+        res.send("hello welcome to Express!");
+    })
+}
 
-app.get('/', (req, res)=>{
-    res.send("hello welcome to Express!");
-})
-
+main()
+.then(() => console.log("Connected to MongoDB!"))
+.catch(err => console.log(err));
 
 app.listen(port, ()=>{
     console.log(`Server running on the port : ${port}`);
