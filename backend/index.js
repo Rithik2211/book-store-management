8b09bcfa5fcc51f61f6bd44340fe5596af6faa80
Rithik2211 <rithikpranao22@gmail.com>
2025-03-21 23:28:30 +0530

    added router setup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bookRoutes = require('./src/books/book.route');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5001;
+const cors = require('cors');
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+//middleware
+app.use(express.json());
+app.use(cors({
+    origin : ['http://localhost:5173/'],
+    credentials : true
+}))
+
+app.use('/api/book', bookRoutes);
 
 async function main() {
     try{
-        await mongoose.connect(process.env.MONGO_URI, clientOptions);
+        await mongoose.connect(process.env.MONGO_URI);
         await mongoose.connection.db.admin().command({ping: 1});
         console.log("You successfully connected to MongoDB!");
     } finally {
@@ -25,19 +34,19 @@ app.listen(port, ()=>{
 })
 
 
-const getUserPosts = async (userId) => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/users/${userId}/posts`, {
-        headers: {
-          'Authorization': `Bearer ${process.env.TOKEN}`,
-          'Content-Type': 'application/json'
-        }
-      });
-      if (!response.ok) throw new Error(`Failed to fetch posts for user ${userId}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(`Error fetching posts for user ${userId}:`, error);
-      throw error;
-    }
-  };
+// const getUserPosts = async (userId) => {
+//     try {
+//       const response = await fetch(`${API_BASE_URL}/users/${userId}/posts`, {
+//         headers: {
+//           'Authorization': `Bearer ${process.env.TOKEN}`,
+//           'Content-Type': 'application/json'
+//         }
+//       });
+//       if (!response.ok) throw new Error(`Failed to fetch posts for user ${userId}`);
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       console.error(`Error fetching posts for user ${userId}:`, error);
+//       throw error;
+//     }
+//   };
